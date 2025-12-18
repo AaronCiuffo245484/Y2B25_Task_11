6305049552bdd6f0fb26dc3b0d76de44e8bbc96c
Nick Belterman 225538 <nick.belterman@outlook.com>
2025-12-18 15:04:18 +0100

    Create nick_ot2_optimizer.py

---
+++ npec_rl/src/nick_ot2_optimizer.py
@@ -0,0 +1,45 @@
+from clearml import Task
+from clearml.automation import HyperParameterOptimizer, DiscreteParameterRange
+from clearml.automation import GridSearch
+from datetime import datetime
+# Generate timestamp for unique task name and model filename
+timestamp = datetime.now().strftime("%y%m%d.%H%M")
+
+PERSON_NAME = "NickBelterman"
+BRANCH_NAME = "batch_size_nick"
+ENTRYPOINT = "nick_train_ot2.py"
+
+task_name = f'OT2_RL_{PERSON_NAME}_{timestamp}'
+# 1. Initialize a Task for the Optimizer itself
+task = Task.init(
+    project_name='Mentor Group - Jason/Group 1',
+    task_name=task_name+"_batch_size_optimization",
+    task_type=Task.TaskTypes.optimizer,
+    reuse_last_task_id=False
+)
+
+# 2. Define the search space
+# We use DiscreteParameterRange because PPO batch sizes should be powers of 2
+param_distribution = [
+    DiscreteParameterRange('Args/batch_size', [128, 256, 512, 1024]),
+    DiscreteParameterRange('Args/n_steps', [2048]) 
+]
+
+# 3. Setup the Optimizer
+optimizer = HyperParameterOptimizer(
+    base_task_id='97a21c7280f74b30985f7fe507c2bc63 ', 
+    hyper_parameters=param_distribution,
+    objective_metric_title='ot2',
+    objective_metric_series='success_rate_100ep',
+    objective_metric_sign='max',
+    optimizer_class=GridSearch,
+    execution_queue='default',
+    max_number_of_concurrent_tasks=1,
+    total_max_jobs=4 
+)
+
+optimizer.start()
+
+optimizer.wait()
+
+optimizer.stop()
